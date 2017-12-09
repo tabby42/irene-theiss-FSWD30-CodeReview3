c2c9d8e5e53689829581e3a53513991cf808c7be
tabby42 <irene.theiss@gmx.at>
2017-12-09 20:53:23 +0100

    add functions for checking input and showing result

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
-var name = document.getElementsByName("name")[0];
+var fname = document.getElementById("full");
 var age = document.getElementsByName("age")[0];
 var country = document.getElementsByName("country")[0];
 var horsepower = document.getElementsByName("horsepower")[0];
 
-
-
 function calculateInsurance() {
-	return parseInt(horsepower.value) * 100 / parseInt(age.value) + 150;
+	return (parseInt(horsepower.value) * 100) / (parseInt(age.value) + 150);
+}
+
+function checkInput () {
+	var user = fname.value;
+	if (user !== "" && age.value !== "" && horsepower.value !== "") {
+		return true;
+	} else {
+		return false;
+	}
 }
 
 function showResult () {
 	var rate = calculateInsurance().toFixed(2);
-	document.getElementById("msg").innerHTML = 
-	"Hi " + String(name.value) + " from " + country.options[country.selectedIndex].text + "!<br>Your rate would be €" + rate + "!";
+	var user = fname.value;
+	if (checkInput() && !isNaN(rate) && rate > 0) {
+		document.getElementById("msg").innerHTML = 
+		"Hi, " + user + " from " + country.options[country.selectedIndex].text 
+		+ "!<br>Your rate would be <span class='orange'>€ " + rate + "</span>";
+	} else {
+		document.getElementById("msg").innerHTML = 
+		"Please fill out all the form fields correctly.";
+	}
 }
 
-
-document.getElementById("submit").addEventListener("click", showResult);
+if (document.getElementById("submit")) {
+	document.getElementById("submit").addEventListener("click", showResult);
+}
